05ae5af2b79b8afd9915c6bfbc63d730bd9e7357
Alvin Hong <alvinhong2014@gmail.com>
2024-09-13 00:15:21 -0700

    Add database and API for links

---
+++ server/api.js
@@ -0,0 +1,31 @@
+const {Router} = require("express");
+const {Links} = require("./db.js");
+
+const apiRouter = Router();
+
+apiRouter.get("/links", async (req, res) => {
+	const links = await Links.findAll();
+	res.json(links);
+});
+
+apiRouter.post("/links", async (req, res) => {
+	const link = await Links.create(req.body);
+	res.json(link);
+});
+
+apiRouter.get("/links/:id", async (req, res) => {
+	const link = await Links.findByPk(req.params.id);
+	res.json(link);
+});
+
+apiRouter.patch("/links/:id", async (req, res) => {
+	await Links.update(req.body, {where: {link_id: req.params.id}});
+	res.status(200).send();
+});
+
+apiRouter.delete("/links/:id", async (req, res) => {
+	await Links.destroy({where: {link_id: req.params.id}});
+	res.status(200).send();
+});
+
+module.exports = apiRouter;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
+const cors = require("cors");
 const express = require("express");
+const apiRouter = require("./api.js");
+const {sequelize} = require("./db.js");
+
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+app.use(cors());
+app.use(express.json());
+app.use("/api", apiRouter);
+
+sequelize.sync().then(() => {
+	app.listen(PORT, () => {
+		console.log(`Server is running on port ${PORT}`);
+	});
 });
