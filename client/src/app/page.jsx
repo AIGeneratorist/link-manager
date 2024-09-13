26d6a3580fa3a7609d0dbb2d5db308e18c419532
Alvin Hong <alvinhong2014@gmail.com>
2024-09-13 16:59:02 -0700

    Add essential client files

---
+++ client/src/app/page.jsx
@@ -0,0 +1,21 @@
+async function getLinks() {
+	const res = await fetch("http://localhost:3000/api/links", {cache: "no-store"});
+	if (!res.ok) {
+		throw new Error("Failed to fetch links");
+	}
+	return res.json();
+}
+
+export default async function Home() {
+	const links = await getLinks();
+	return (
+		<>
+			<h1>Link Manager</h1>
+			<ul>
+				{links.map(link => (
+					<li key={link.link_id}>{link.url}</li>
+				))}
+			</ul>
+		</>
+	);
+}
