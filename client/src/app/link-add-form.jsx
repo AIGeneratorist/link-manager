86c38bb09864ef20ddcc80abd5f08b6acfaf3cd5
Alvin Hong <alvinhong2014@gmail.com>
2024-09-21 16:09:04 -0700

    Add more link properties to client

--- client/src/app/link-add-form.jsx
+++ client/src/app/link-add-form.jsx
@@ -2,8 +2,25 @@
 
 import {useState} from "react";
 
+const Priority = {
+	LOW: 1,
+	MEDIUM: 2,
+	HIGH: 3
+};
+
 export default function LinkAddForm() {
-	const [url, setURL] = useState("");
+	const [inputData, setInputData] = useState({
+		url: "",
+		title: "",
+		folder: "",
+		category: "",
+		type: "",
+		found_at: "",
+		read_at: "",
+		priority: Priority.MEDIUM,
+		favorited: false,
+		comments: ""
+	});
 
 	async function handleSubmit(ev) {
 		ev.preventDefault();
@@ -13,14 +30,34 @@ export default function LinkAddForm() {
 			headers: {
 				"Content-Type": "application/json"
 			},
-			body: JSON.stringify({url})
+			body: JSON.stringify({
+				...inputData,
+				title: inputData.title || null,
+				folder: inputData.folder || null,
+				category: inputData.category || null,
+				type: inputData.type || null,
+				found_at: inputData.found_at || null,
+				read_at: inputData.read_at || null,
+				comments: inputData.comments || null
+			})
 		});
 
 		if (!res.ok) {
 			throw new Error("Failed to add link");
 		}
 
-		setURL("");
+		setInputData({
+			url: "",
+			title: "",
+			folder: "",
+			category: "",
+			type: "",
+			found_at: "",
+			read_at: "",
+			priority: Priority.MEDIUM,
+			favorited: false,
+			comments: ""
+		});
 	}
 
 	return (
@@ -29,9 +66,84 @@ export default function LinkAddForm() {
 			<input
 				type="text"
 				name="url"
-				value={url}
-				onChange={ev => setURL(ev.target.value)}
+				value={inputData.url}
+				onChange={ev => setInputData({...inputData, url: ev.target.value})}
 			/><br />
+
+			Title:
+			<input
+				type="text"
+				name="title"
+				value={inputData.title}
+				onChange={ev => setInputData({...inputData, title: ev.target.value})}
+			/><br />
+
+			Folder:
+			<input
+				type="text"
+				name="folder"
+				value={inputData.folder}
+				onChange={ev => setInputData({...inputData, folder: ev.target.value})}
+			/><br />
+
+			Category:
+			<input
+				type="text"
+				name="category"
+				value={inputData.category}
+				onChange={ev => setInputData({...inputData, category: ev.target.value})}
+			/><br />
+
+			Type:
+			<input
+				type="text"
+				name="type"
+				value={inputData.type}
+				onChange={ev => setInputData({...inputData, type: ev.target.value})}
+			/><br />
+
+			Found Date:
+			<input
+				type="date"
+				name="found_at"
+				value={inputData.found_at}
+				onChange={ev => setInputData({...inputData, found_at: ev.target.value})}
+			/><br />
+
+			Read Date:
+			<input
+				type="date"
+				name="read_at"
+				value={inputData.read_at}
+				onChange={ev => setInputData({...inputData, read_at: ev.target.value})}
+			/><br />
+
+			Priority:
+			<select
+				name="priority"
+				value={inputData.priority}
+				onChange={ev => setInputData({...inputData, priority: ev.target.value})}
+			>
+				<option value={Priority.LOW}>Low</option>
+				<option value={Priority.MEDIUM}>Medium</option>
+				<option value={Priority.HIGH}>High</option>
+			</select><br />
+
+			Favorite:
+			<input
+				type="checkbox"
+				name="favorited"
+				checked={inputData.favorited}
+				onChange={ev => setInputData({...inputData, favorited: ev.target.checked})}
+			/><br />
+
+			Comments:
+			<textarea
+				name="comments"
+				value={inputData.comments}
+				onChange={ev => setInputData({...inputData, comments: ev.target.value})}
+			/><br />
+
 			<button type="submit">Add</button>
 		</form>
 	);
--- client/src/app/link/[id]/page.jsx
+++ client/src/app/link/[id]/page.jsx
@@ -1,3 +1,5 @@
+const priorities = [null, "Low", "Medium", "High"];
+
 async function getLink(id) {
 	const res = await fetch(`http://localhost:3000/api/links/${id}`, {cache: "no-store"});
 	if (!res.ok) {
@@ -11,8 +13,19 @@ export default async function LinkView({params}) {
 	return (
 		<>
 			<h1>View Link</h1>
+
 			<p><a href={link.url} target="_blank">{link.url}</a></p>
+
 			<p>ID: {link.link_id}</p>
+			<p>Title: {link.title || <i>No title</i>}</p>
+			<p>Folder: {link.folder || <i>No folder</i>}</p>
+			<p>Category: {link.category || <i>No category</i>}</p>
+			<p>Type: {link.type || <i>No type</i>}</p>
+			<p>Found Date: {link.found_at || <i>No found date</i>}</p>
+			<p>Read Date: {link.read_at || <i>No read date</i>}</p>
+			<p>Priority: {priorities[link.priority] || <i>No priority</i>}</p>
+			<p>Favorite: {link.favorited ? "Yes" : "No"}</p>
+			<p>Comments: {link.comments || <i>No comments</i>}</p>
 		</>
 	);
 }
