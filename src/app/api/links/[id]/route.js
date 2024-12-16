d9d7410ef8e537e25407b4e3c0c2d6e3c7d46273
Alvin Hong <alvinhong2014@gmail.com>
2024-12-16 09:46:53 -0800

    Improve API error handling

--- src/app/api/links/[id]/route.js
+++ src/app/api/links/[id]/route.js
@@ -1,17 +1,60 @@
+import {DatabaseError} from "sequelize";
 import {Links} from "@/db/db.js";
 
 export const GET = async (req, {params}) => {
-	const link = await Links.findByPk(params.id);
-	return Response.json(link);
+	try {
+		const link = await Links.findByPk(params.id);
+		if (!link) {
+			return Response.json({error: "Link not found"}, {status: 404});
+		}
+		return Response.json(link);
+	} catch (err) {
+		if (err instanceof DatabaseError && err.message.startsWith("invalid input syntax")) {
+			return Response.json({error: "Invalid ID"}, {status: 404});
+		}
+		return Response.json({error: `Server error: ${err}`}, {status: 500});
+	}
 };
 
 export const PATCH = async (req, {params}) => {
-	const body = await req.json();
-	await Links.update(body, {where: {link_id: params.id}});
-	return new Response();
+	const parsedId = parseInt(params.id);
+	if (isNaN(parsedId)) {
+		return Response.json({error: "Invalid ID"}, {status: 404});
+	}
+
+	try {
+		const body = await req.json();
+
+		const tempLink = Links.build(body);
+		if (!tempLink.changed()) {
+			return Response.json({error: "Invalid field name(s)"}, {status: 400});
+		}
+
+		const [updatedCount] = await Links.update(body, {where: {link_id: parsedId}});
+		if (updatedCount == 0) {
+			return Response.json({error: "Link not found"}, {status: 404});
+		}
+
+		return new Response();
+	} catch (err) {
+		if (err instanceof DatabaseError && err.message.startsWith("invalid input syntax")) {
+			return Response.json({error: `Invalid field value: ${err}`}, {status: 400});
+		}
+		return Response.json({error: `Server error: ${err}`}, {status: 500});
+	}
 };
 
 export const DELETE = async (req, {params}) => {
-	await Links.destroy({where: {link_id: params.id}});
-	return new Response();
+	try {
+		const deletedCount = await Links.destroy({where: {link_id: params.id}});
+		if (deletedCount == 0) {
+			return Response.json({error: "Link not found"}, {status: 404});
+		}
+		return new Response();
+	} catch (err) {
+		if (err instanceof DatabaseError && err.message.startsWith("invalid input syntax")) {
+			return Response.json({error: "Invalid ID"}, {status: 404});
+		}
+		return Response.json({error: `Server error: ${err}`}, {status: 500});
+	}
 };
--- src/app/api/links/route.js
+++ src/app/api/links/route.js
@@ -1,12 +1,27 @@
+import {DatabaseError, UniqueConstraintError} from "sequelize";
 import {Links} from "@/db/db.js";
 
 export const GET = async () => {
-	const links = await Links.findAll();
-	return Response.json(links);
+	try {
+		const links = await Links.findAll();
+		return Response.json(links);
+	} catch (err) {
+		return Response.json({error: `Server error: ${err}`}, {status: 500});
+	}
 };
 
 export const POST = async req => {
-	const body = await req.json();
-	const link = await Links.create(body);
-	return Response.json(link);
+	try {
+		const body = await req.json();
+		const link = await Links.create(body);
+		return Response.json(link);
+	} catch (err) {
+		if (err instanceof UniqueConstraintError) {
+			return Response.json({error: "Link already exists"}, {status: 400});
+		}
+		if (err instanceof DatabaseError && err.message.startsWith("invalid input syntax")) {
+			return Response.json({error: `Invalid field value: ${err}`}, {status: 400});
+		}
+		return Response.json({error: `Server error: ${err}`}, {status: 500});
+	}
 };
