469c4e4a1360096de1fbbdf504f5381dc1a9791c
Alvin Hong <alvinhong2014@gmail.com>
2025-05-25 09:48:57 -0700

    Move query parameter parsing to util

--- src/app/api/links/route.js
+++ src/app/api/links/route.js
@@ -1,59 +1,14 @@
 import {Links} from "@/db/db.js";
-import {errorToResponse} from "@/utils/utils.js";
+import {errorToResponse, parseQueryParams} from "@/utils/utils.js";
 
 export const GET = async req => {
-	const rawPage = req.nextUrl.searchParams.get("page");
-	let page;
-	if (rawPage) {
-		page = parseInt(rawPage);
-		if (isNaN(page) || page < 1) {
-			return Response.json({error: "Invalid page"}, {status: 400});
-		}
-	} else {
-		page = 1;
-	}
-
-	const rawLimit = req.nextUrl.searchParams.get("limit");
-	let limit;
-	if (rawLimit) {
-		limit = parseInt(rawLimit);
-		if (isNaN(limit) || limit < 1) {
-			return Response.json({error: "Invalid limit"}, {status: 400});
-		}
-	} else {
-		limit = 25;
-	}
-
-	const rawSort = req.nextUrl.searchParams.get("sort");
-	let sortField;
-	let sortOrder;
-	if (rawSort) {
-		if (rawSort.endsWith("Desc")) {
-			sortField = rawSort.slice(0, -4);
-			sortOrder = "DESC";
-		} else if (rawSort.endsWith("Asc")) {
-			sortField = rawSort.slice(0, -3);
-			sortOrder = "ASC";
-		} else {
-			sortField = rawSort;
-			sortOrder = "ASC";
-		}
-
-		const attributes = Links.getAttributes();
-		if (!attributes[sortField]) {
-			return Response.json({error: "Invalid sort field"}, {status: 400});
-		}
-	} else {
-		sortField = "createdAt";
-		sortOrder = "DESC";
+	const parseRes = parseQueryParams(req, Links);
+	if (parseRes.error) {
+		return Response.json(parseRes, {status: 400});
 	}
 
 	try {
-		const {rows, count} = await Links.findAndCountAll({
-			limit,
-			offset: (page - 1) * limit,
-			order: [[sortField, sortOrder]]
-		});
+		const {rows, count} = await Links.findAndCountAll(parseRes.data);
 		return Response.json({results: rows, count});
 	} catch (err) {
 		return errorToResponse(err);
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,61 @@
 import {DatabaseError, UniqueConstraintError, ValidationError} from "sequelize";
 
+export function parseQueryParams(req, model) {
+	const rawPage = req.nextUrl.searchParams.get("page");
+	let page;
+	if (rawPage) {
+		page = parseInt(rawPage);
+		if (isNaN(page) || page < 1) {
+			return {error: "Invalid page"};
+		}
+	} else {
+		page = 1;
+	}
+
+	const rawLimit = req.nextUrl.searchParams.get("limit");
+	let limit;
+	if (rawLimit) {
+		limit = parseInt(rawLimit);
+		if (isNaN(limit) || limit < 1) {
+			return {error: "Invalid limit"};
+		}
+	} else {
+		limit = 25;
+	}
+
+	const rawSort = req.nextUrl.searchParams.get("sort");
+	let sortField;
+	let sortOrder;
+	if (rawSort) {
+		if (rawSort.endsWith("Desc")) {
+			sortField = rawSort.slice(0, -4);
+			sortOrder = "DESC";
+		} else if (rawSort.endsWith("Asc")) {
+			sortField = rawSort.slice(0, -3);
+			sortOrder = "ASC";
+		} else {
+			sortField = rawSort;
+			sortOrder = "ASC";
+		}
+
+		const attributes = model.getAttributes();
+		if (!attributes[sortField]) {
+			return {error: "Invalid sort field"};
+		}
+	} else {
+		sortField = "createdAt";
+		sortOrder = "DESC";
+	}
+
+	return {
+		data: {
+			limit,
+			offset: (page - 1) * limit,
+			order: [[sortField, sortOrder]]
+		}
+	};
+}
+
 export function errorToResponse(err) {
 	if (err instanceof UniqueConstraintError) {
 		return Response.json(
